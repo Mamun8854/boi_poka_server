b593d0028ca2e23f473e9417620e158e16041812
Mamun8854 <mamunabdullah8854@gmail.com>
2022-11-25 02:26:04 +0600

    users info post to db

--- index.js
+++ index.js
@@ -25,11 +25,40 @@ async function run() {
   try {
     // categories collection
     const categoriesCollection = client.db("BoiPoka").collection("categories");
-
+    // books collection
+    const booksCollection = client.db("BoiPoka").collection("books");
+    // users collection
+    const usersCollection = client.db("BoiPoka").collection("users");
+    // category data get api
     app.get("/category", async (req, res) => {
       const query = {};
       const result = await categoriesCollection.find(query).toArray();
       res.send(result);
+    });
+
+    // books data get and get category wise books api by id
+
+    app.get("/category/:id", async (req, res) => {
+      const query = req.params.id;
+      const filter = { category: query };
+      const result = await booksCollection.find(filter).toArray();
+      res.send(result);
+    });
+
+    // new user
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const userEmail = user.email;
+      const query = { email: userEmail };
+      const newUser = await usersCollection.find(query).toArray();
+
+      if (newUser.length > 0) {
+        return res.send("User Already Have");
+      } else {
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      }
     });
   } finally {
   }
